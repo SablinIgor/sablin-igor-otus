16cdfc7e015193cd2cd3eb74ab339fb274eb6899
Sablin Igor <sablin.igor@gmail.com>
2018-07-25 19:51:40 +0300

    Обработка исключения при передаче параметра отличного от числа

--- closure_sum.js
+++ closure_sum.js
@@ -1,4 +1,23 @@
 /**
+ * Формирование пользовательского исключения.
+ * @param {string} a - Текст сообщения об ошибке
+ */
+function UserException(message) {
+    this.message = message;
+    this.name = "Javascript Lesson 1 Exception";
+}
+
+/**
+ * Проверка на корректность переданного аргумента.
+ * @param {any} a - Переданный в функцию sum параметр
+ */
+function checkParam(a) {
+    if (typeof(a) != "number") {
+        throw new UserException("Передано значение отличное от числа: " + a)
+    }
+}
+
+/**
  * Функция sum, которая может быть исполнена множество раз.
  * Если она исполнена без аргументов, то возвращает значение суммы всех переданных до этого значений.
  *
@@ -9,14 +28,22 @@
  */
 function sum(a) {
     let count = a;
+
+    checkParam(a);
+
     return function by(b) {
         if (arguments.length === 0) {
             return count;
         } else {
+            checkParam(b);
             count += b;
             return by;
         }
     }
 }
 
-console.log(sum(1)(2)(4)())
+/*try {
+    console.log(sum(1)(4)(4)())
+} catch (e) {
+    console.log(e)
+}*/
