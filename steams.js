34f5a20a72238e5486b2640cf8061197fd715342
Igor <sablin.igor@gmail.com>
2018-08-22 20:19:11 +0300

    Add highWaterMark

--- steams.js
+++ steams.js
@@ -8,6 +8,9 @@ const { Readable } = require('stream');
 const { Writable } = require('stream');
 const { Transform } = require('stream');
 
+let count = 0;
+let start = new Date;
+
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -18,7 +21,14 @@ function getRandomInt(min, max) {
 const Generator = new Readable({
     read() {
         this.push(getRandomInt(0, 1000).toString())
-    }
+        count++;
+        if (count > 500000) {
+            var end = new Date; // конец измерения
+            console.log( "Цикл занял " + (end - start) + " ms" );
+            process.kill(process.pid);
+        }
+    },
+    highWaterMark: 1
 });
 
 /*
@@ -28,7 +38,8 @@ const Transformation = new Transform({
     transform(chunk, encoding, callback) {
         this.push((parseInt(chunk.toString()) + getRandomInt(0, 1000)).toString());
         callback();
-    }
+    },
+    highWaterMark: 10
 });
 
 /*
@@ -38,8 +49,13 @@ const Printer = new Writable({
     write(chunk, encoding, callback) {
         console.log(chunk.toString());
         callback();
-    }
+    },
+    highWaterMark: 2
 });
+
+setTimeout((function() {
+    return process.kill(process.pid);
+}), 5000);
 
 /*
 Запуск потока
