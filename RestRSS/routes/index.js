2b6eadeb146039f13b5406f47b5ecda165f43265
Igor <sablin.igor@gmail.com>
2018-09-03 11:37:58 +0300

    Реализация входной точки: Создание рассылки по RSS

--- RestRSS/routes/index.js
+++ RestRSS/routes/index.js
@@ -1,12 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const db = require("../database/mongodb");
+const router = express.Router();
+
+// RSS parser
+let Parser = require('rss-parser');
+let parser = new Parser();
 
 router.get('/rss', function (req, res) {
     res.send("Показ списка всех добавленных RSS рассылок");
 })
 
 router.post('/rss', function (req, res) {
-    res.status(201).send("Создание рассылки по RSS");
+
+    parser.parseURL(req.body.rss).then(feed =>{
+
+        // save rss
+        const newRSS = {
+            url: req.body.rss,
+            created: (new Date()).getTime(),
+            pubdate: feed.pubDate,
+            documents: feed.items
+        }
+
+        const rssCollection = db.get('rss_feed');
+
+        rssCollection.insert(newRSS)
+            .then((res) => {
+                console.log("RSS saved!")
+                res.status(201).send("OK");
+            })
+            .catch((err) => {
+                console.log(
+                    "ERROR! Something went wrong: " + err.errmsg
+                )
+                res.status(500).send(err.errmsg);
+            })
+
+    })
+
 })
 
 router.get('/rss/:id', function (req, res) {
